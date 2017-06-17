25f6d7af7fd495c9c4bb50661a3e1e493a6a7299
szabloreki <oscar1tolowski@gmail.com>
2017-06-17 17:35:30 +0200

    Added adding teams

--- routing/teams/details.js
+++ routing/teams/details.js
@@ -1,21 +1,45 @@
 const Teams = require('../../models/teams');
-
+const validator = require('validator');
 
 
 exports.check = (req, res) =>{ res.status(200).json('Ok')}
 
 exports.createTeam = (req, res) =>{
+
+  let error = false;
+  let info = []
+  let name = validator.isLength(req.body.name, {min:5, max: 10})
+
+  if(!name){
+    return res.status(404).json("You must use name of team beetween 5 and 10 char!")
+  }
+
+  console.log(req.body.users)
+  for(pe in req.body.users){
+    console.log(pe.userID)
+  }
+
+  for(user of req.body.users){
+    console.log(user.userID);
+    Teams.find({"_id" : user.userID}, (err, date)=>{
+          if(err){
+            error = true;
+            console.log(date);
+          }
+          else {
+            console.log('Znalazlo!');
+            console.log(date);
+          }
+    })
+  }
+
+  if(error){
+    return res.status(404).json('One or more users have a bad ID!');
+  }
+  else {
     let team = new Teams();
-
-    team.name = "New young"
-
-    team.users[0] = {
-      personId: "342423423",
-      firstName: "Kasia",
-      lastName: "Grabara",
-      email: "@twoja"
-    }
-
+    team.name = req.body.name;
+    team.users = req.body.users;
     team.save((err, date)=>{
           if(err){
             res.status(500).json(err);
@@ -25,5 +49,12 @@ exports.createTeam = (req, res) =>{
             res.status(200).json(date);
           }
     })
+  }
 };
-exports.check = (req, res) =>{ res.status(200).json('Ok')}
+
+
+
+
+exports.findAllTeams = (req, res) =>{
+
+}
