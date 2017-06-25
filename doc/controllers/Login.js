9876f609b737ce8988213686a6c87655e88d86bc
szabloreki <oscar1tolowski@gmail.com>
2017-06-25 21:59:30 +0200

    Delete Login.js

--- doc/controllers/Login.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var url = require('url');
-
-var Login = require('./LoginService');
-
-module.exports.appUsersLoginGET = function appUsersLoginGET (req, res, next) {
-  Login.appUsersLoginGET(req.swagger.params, res, next);
-};
